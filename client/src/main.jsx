f12eed2bde45b68a5e942b1a6370a3ce1bb80e41
lbo-development <lbo-development@outlook.fr>
2026-03-31 18:27:14 +0200

    refactor(ui): finalize sprint 1 styling and feedback polish

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
+import "./tokens.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
